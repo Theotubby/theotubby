f9ec511153f2bd2a294bf9f728280ac9750c70c4
Theotubby <theodortran2002@gmail.com>
2025-06-09 19:03:23 +0200

    ver. 0.1.8.2

--- assets/home/js/header.js
+++ assets/home/js/header.js
@@ -9,6 +9,43 @@ const sunIcon = document.getElementById("sun-icon");
 const moonIcon = document.getElementById("moon-icon");
 
 const header = document.getElementById("header-container");
+const navi_button_container = document.getElementById("navi-button-container");
+
+function updatePadding() {
+    const width = window.innerWidth;
+
+    if (width >= 300) {
+        let paddingVW = 1.4;
+
+        if (width < 1024) {
+            const steps = Math.floor((width - 300) / 100);
+            paddingVW += steps * 0.5;
+        } else if (width > 1024 && width < 1500){
+            const baseSteps = Math.floor((1024 - 300) / 100);
+            paddingVW += baseSteps * 0.5;
+
+            let extraSteps = Math.floor((width - 1024) / 100);
+            let scaling = 0.325;
+            for (let i = 0; i < extraSteps; i++) {
+                paddingVW += scaling;
+                scaling = Math.max(0, scaling - 0.035); // wird immer um 0.01 kleiner, nie negativ
+            }
+        } else if (width > 1500){
+            const baseSteps = Math.floor((1024 - 300) / 100);
+            paddingVW += baseSteps * 0.325;
+
+            let extraSteps = Math.floor((width - 1024) / 100);
+            let scaling = 0.4;
+            for (let i = 0; i < extraSteps; i++) {
+                paddingVW += scaling;
+                scaling = Math.max(0, scaling - 0.025); // wird immer um 0.01 kleiner, nie negativ
+            }
+        }
+        navi_button_container.style.paddingLeft = paddingVW + 'vw';
+    } else {
+        navi_button_container.style.paddingLeft = '0vw';
+    }
+}
 
 window.addEventListener("resize", function() {
     if(window.innerWidth >= 940){
@@ -16,6 +53,7 @@ window.addEventListener("resize", function() {
         header.classList.remove("expanded");
         body.classList.remove("no-scroll");
     }
+    updatePadding();
 });
 
 window.addEventListener("scroll", function() {
@@ -28,7 +66,6 @@ window.addEventListener("scroll", function() {
     }
 });
 
-
 navi_dropdown_checkbox.addEventListener("change", function() {
     if (navi_dropdown_checkbox.checked){
         header.classList.add("expanded");
@@ -40,6 +77,9 @@ navi_dropdown_checkbox.addEventListener("change", function() {
         body.classList.remove("no-scroll");
     }
 });
+
+// Initial ausführen
+updatePadding();
 
 
 
